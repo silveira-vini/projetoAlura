daec5ad4d485f481ec394c451a2b45ee2bb753cf
Vinicius Silveira <vsribeiro87@gmail.com>
2024-09-17 22:44:45 -0300

    ProgressingJavaAlura

---
+++ src/javaPOO/sharedBehaviour/models/Audio.java
@@ -0,0 +1,39 @@
+package javaPOO.sharedBehaviour.models;
+
+public class Audio {
+
+    private String titulo;
+    private int totalDeReproducoes;
+    private int curtidas;
+    private int classificacao;
+
+    public void curtir() {
+        this.curtidas++;
+    }
+
+    public void reproduzir() {
+        this.totalDeReproducoes++;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getTotalDeReproducoes() {
+        return totalDeReproducoes;
+    }
+
+    public int getCurtidas() {
+        return curtidas;
+    }
+
+    public int getClassificacao() {
+        return classificacao;
+    }
+
+}
+
