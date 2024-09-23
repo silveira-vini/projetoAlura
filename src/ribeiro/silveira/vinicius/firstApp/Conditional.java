f4825e7c6658b8e51379cd807b99b68b09118fdf
Vinicius Silveira <vsribeiro87@gmail.com>
2024-09-23 09:06:56 -0300

    Update Conditional.java

--- src/ribeiro/silveira/vinicius/firstApp/Conditional.java
+++ src/ribeiro/silveira/vinicius/firstApp/Conditional.java
@@ -6,13 +6,13 @@ public class Conditional {
 
         int year = 2024;
         boolean inPlan = true;
-        double grade = 8.5;
+        double grade1 = 8.5;
 
 
         if (year > 2022 && inPlan) {
             System.out.println("Filme novo");
         } else {
-            grade = 5.8;
+            grade1 = 5.8;
             System.out.println("Filme antigo");
         }
 
